f0ebbc8c67da817c2e32a556e2fc0a575402e53d
Abdelsatar <abdelsatarahmed77777@gmail.com>
2023-08-17 14:31:20 +0300

    main function update

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
-    cout << "Hello world!" << endl;
+    cout << "welcome git !" << endl;
     return 0;
 }
